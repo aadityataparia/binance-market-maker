82d77f48a620ca6f73240c16c62c8c6a03dd37fc
Aaditya Taparia <aadityataparia@gmail.com>
2022-12-11 03:01:41 +0900

    refactor: socket client improvements

--- src/SocketClient/index.ts
+++ src/SocketClient/index.ts
@@ -1,26 +1,33 @@
-type JSONValue =
-    | string
-    | JSONObject;
+import WebSocket from 'ws';
+
+type JSONValue = string | number | JSONObject;
 
 interface JSONObject {
-    id?: string;
-    [x: string]: JSONValue;
+  id?: number;
+  [x: string]: JSONValue | JSONValue[];
 }
 
 class SocketClient {
   _requests = {};
 
   url: string;
-  protocol: string;
   ws: WebSocket;
-  private _fallback: boolean;
+  protocol?: string;
+  onmessage?: (event: MessageEvent<any>) => void;
 
   constructor(
     url: string,
-    protocol: string
+    {
+      protocol,
+      onmessage
+    }: {
+      protocol?: string;
+      onmessage?: (event: MessageEvent<any>) => void;
+    } = {}
   ) {
     this.url = url;
     this.protocol = protocol;
+    this.onmessage = onmessage;
   }
 
   connect() {
@@ -32,56 +39,53 @@ class SocketClient {
   }
 
   send(message: JSONValue) {
-    const id = typeof message === "object" ? message?.id : null;
+    const id = typeof message === 'object' ? message?.id : null;
 
-    return this._openSocket()
-      .then((ws: { send: (arg0: any) => void }) => {
-        ws.send(message);
-        return new Promise(res => {
-          if (id) {
-            this._requests[id] = (v: unknown) => {
-              delete this._requests[id];
-              res(v);
-            }
-          }
-        });
+    return this._openSocket().then((ws: { send: (arg0: any) => void }) => {
+      ws.send(JSON.stringify(message));
+      return new Promise((res) => {
+        if (id) {
+          this._requests[id] = (v: unknown) => {
+            delete this._requests[id];
+            res(v);
+          };
+        }
       });
+    });
   }
 
   _openSocket() {
     if (!this.ws) {
       this.ws = new WebSocket(this.url, this.protocol);
       this.ws.onmessage = this._onmessage.bind(this);
+      this.ws.onerror = this.onerror;
     } else if (this.ws.readyState === this.ws.OPEN) {
       return Promise.resolve(this.ws);
     } else if (this.ws.readyState !== this.ws.CONNECTING) {
       this.ws = null;
       return this._openSocket();
     }
-    const me = this;
     return new Promise((res, rej) => {
-      function waiting() {
-        if (me.ws.readyState === me.ws.CONNECTING) {
+      const waiting = () => {
+        if (this.ws.readyState === this.ws.CONNECTING) {
           setTimeout(waiting, 100);
-        } else if (me.ws.readyState !== me.ws.OPEN) {
-          rej(Error(`Failed to open socket on ${me.url}`));
+        } else if (this.ws.readyState !== this.ws.OPEN) {
+          rej(Error(`Failed to open socket on ${this.url}`));
         } else {
-          res(me.ws);
+          res(this.ws);
         }
-      }
+      };
       waiting();
     });
   }
 
   _onmessage(event: MessageEvent<any>) {
     const data = JSON.parse(event.data);
-    if (data.id && this._requests[data.id]) {
-      this._requests[data.id].res(data.payload);
-    }
-    this.onmessage(event);
+    if (data.id) this._requests[data.id]?.(data.payload);
+    this.onmessage?.(event);
   }
 
-  onmessage(e: { data: string }) {}
+  onerror() {}
 }
 
 export default SocketClient;
